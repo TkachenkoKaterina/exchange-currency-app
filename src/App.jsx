cba5217af407848cb51fb3084e8a8ce1eda8fd0a
Andrii Krupa <krupaandrii@gmail.com>
2024-03-17 10:05:53 +0100

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-import { Heading } from 'components';
+import { Header } from 'components';
+import Home from 'pages/Home';
+import Rates from 'pages/Rates';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 export const App = () => {
-  return <Heading title="Just do it!" />;
+  return (
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Home />} />
+        <Route path="/rates" element={<Rates />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
 };
